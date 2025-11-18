9e9fb91e1f2c06a23cf7fc1a03cc85d8c469a67c
jorge212313cdfdws <jorgedelaparra0@gmail.com>
2025-11-18 15:56:42 +0000

    cambio de la clase cliente (modelo y controlador)

--- src/main/java/com/taller/mecanica/model/Cliente.java
+++ src/main/java/com/taller/mecanica/model/Cliente.java
@@ -1,7 +1,7 @@
 package com.taller.mecanica.model;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity; 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,15 +13,13 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Cliente {
 
-    @Id 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id; 
+    private long id;
 
-    @Column (nullable = false)
-    private String nombre; 
+    @Column(nullable = false)
+    private String nombre;
 
-    
 }
